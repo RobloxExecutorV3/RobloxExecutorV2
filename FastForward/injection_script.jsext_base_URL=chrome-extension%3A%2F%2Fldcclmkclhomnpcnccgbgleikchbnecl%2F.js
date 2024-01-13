d8b90f5bc52b88a0239ba8607292bbc5fbe147f6
RobloxExecutorV3 <155648690+RobloxExecutorV3@users.noreply.github.com>
2024-01-13 19:04:29 +0300

    Add files via upload

---
+++ FastForward/injection_script.jsext_base_URL=chrome-extension%3A%2F%2Fldcclmkclhomnpcnccgbgleikchbnecl%2F.js
@@ -0,0 +1,159 @@
+const bypasses = {
+    "1link.club": "bypasses/1link.js",
+    "1shortlink.com": "bypasses/1shortlink.js",
+    "acortalink.me": "bypasses/acortalink.js",
+    "mega-guy.com": "bypasses/admaven.js",
+    "ofpacksmega.com": "bypasses/admaven.js",
+    "depravityweb.co": "bypasses/admaven.js",
+    "secretpack-links.com": "bypasses/admaven.js",
+    "secret-links.com": "bypasses/admaven.js",
+    "tavernleaks.com": "bypasses/admaven.js",
+    "free-leaks.com": "bypasses/admaven.js",
+    "hotstars-leaks.com": "bypasses/admaven.js",
+    "thepremium.online": "bypasses/admaven.js",
+    "admiregirls-byme.com": "bypasses/admaven.js",
+    "all-fans.online": "bypasses/admaven.js",
+    "pnp-drops.me": "bypasses/admaven.js",
+    "megadropz.com": "bypasses/admaven.js",
+    "goldmega.online": "bypasses/admaven.js",
+    "badgirlsdrop.com": "bypasses/admaven.js",
+    "rareofhub.com": "bypasses/admaven.js",
+    "only-fun.xyz": "bypasses/admaven.js",
+    "megadumpz.com": "bypasses/admaven.js",
+    "leakutopia.site": "bypasses/admaven.js",
+    "xprmpacks.com": "bypasses/admaven.js",
+    "onlymega.co": "bypasses/admaven.js",
+    "tomxcontent.com": "bypasses/admaven.js",
+    "newsociety0.co": "bypasses/admaven.js",
+    "cemendemons.com": "bypasses/admaven.js",
+    "fansmega.com": "bypasses/admaven.js",
+    "premiumstashdrop.com": "bypasses/admaven.js",
+    "akoam.to": "bypasses/akoam.js",
+    "akwam.org": "bypasses/akwam.js",
+    "akw.to": "bypasses/akwam.js",
+    "an1.com": "bypasses/an1.js",
+    "anonym.ninja": "bypasses/anonym.js",
+    "blitly.io": "bypasses/blitly.js",
+    "boost.ink": "bypasses/boost.js",
+    "brpaper.com": "bypasses/brpaper.js",
+    "bstlar.com": "bypasses/bstlar.js",
+    "cb.run": "bypasses/cbrun.js",
+    "cb.click": "bypasses/cbrun.js",
+    "cheatsquad.gg": "bypasses/cheatsquad.js",
+    "www.dlink2.com": "bypasses/clictune.js",
+    "complete2unlock.com": "bypasses/complete2unlock.js",
+    "cpmlink.net": "bypasses/cpmlink.js",
+    "crackedappsstore.com": "bypasses/crackedappsstore.js",
+    "daominhha.com/download": "bypasses/daominhha.js",
+    "earnme.club": "bypasses/earnme.js",
+    "usanewstoday.club": "bypasses/earnme.js",
+    "fc-lc.com": "bypasses/fclc.js",
+    "filedm.com": "bypasses/filedm.js",
+    "filehorse.com": "bypasses/filehorse.js",
+    "forex1pro.com": "bypasses/forex1pro.js",
+    "4shared.com": "bypasses/fourshared.js",
+    "fssquad.com": "bypasses/fssquad.js",
+    "gamesmega.net": "bypasses/gamesmega.js",
+    "gixen.com/home_1.php": "bypasses/gixen.js",
+    "leitup.com": "bypasses/leitup.js",
+    "letsboost.net": "bypasses/letsboost.js",
+    "liblink.pl": "bypasses/liblink.js",
+    "linegee.net": "bypasses/linegee.js",
+    "linksht.com": "bypasses/linksht.js",
+    "linkspy.cc": "bypasses/linkspy.js",
+    "linkvertise.com": "bypasses/linkvertise.js",
+    "linkvertise.net": "bypasses/linkvertise.js",
+    "link-to.net": "bypasses/linkvertise.js",
+    "lnk.parts": "bypasses/lnk.js",
+    "icerik.site": "bypasses/lnk.js",
+    "lnk2.cc": "bypasses/lnk2cc.js",
+    "longfiles.com": "bypasses/longfiles.js",
+    "filepuma.com": "bypasses/longfiles.js",
+    "portableapps.com": "bypasses/longfiles.js",
+    "indishare.org": "bypasses/longfiles.js",
+    "solvetube.site": "bypasses/longfiles.js",
+    "mangalist.org": "bypasses/mangalist.js",
+    "mobi2c.com": "bypasses/mobi2c.js",
+    "newforex.online": "bypasses/mobi2c.js",
+    "healthy4pepole.com": "bypasses/mobi2c.js",
+    "world-trips.net": "bypasses/mobi2c.js",
+    "forex-gold.net": "bypasses/mobi2c.js",
+    "healdad.com": "bypasses/mobi2c.js",
+    "world2our.com": "bypasses/mobi2c.js",
+    "gamalk-sehetk.com": "bypasses/mobi2c.js",
+    "mobitaak.com": "bypasses/mobi2c.js",
+    "forexit.online": "bypasses/mobi2c.js",
+    "shopforex.online": "bypasses/mobi2c.js",
+    "bluetechno.net": "bypasses/mobi2c.js",
+    "mydramalist.com": "bypasses/mydramalist.js",
+    "oko.sh": "bypasses/oko.js",
+    "onepieceex.net": "bypasses/onepieceex.js",
+    "online-fix.me/ext/": "bypasses/onlinefix.js",
+    "oracle.com": "bypasses/oracle.js",
+    "ouo.press": "bypasses/ouo.js",
+    "ouo.io": "bypasses/ouo.js",
+    "oxy.cloud": "bypasses/oxy.js",
+    "dl.pcgamestorrents.org": "bypasses/pcgamestorrents.js",
+    "pirateproxy.wtf": "bypasses/pirateproxy.js",
+    "rekonise.com": "bypasses/rekonise.js",
+    "sfile.mobi": "bypasses/sfile.js",
+    "sfile.xyz": "bypasses/sfile.js",
+    "apkmos.com": "bypasses/sfile.js",
+    "links.shortenbuddy.com": "bypasses/shortenbuddy.js",
+    "shortly.xyz": "bypasses/shortly.js",
+    "shortmoz.link": "bypasses/shortmoz.js",
+    "/sourceforge.net/projects/.+/files/.+/download/": "bypasses/sourceforge.js",
+    "squidssh.com/li/go.php": "bypasses/squidssh.js",
+    "goodssh.com/li/go.php": "bypasses/squidssh.js",
+    "srt.am": "bypasses/srtam.js",
+    "sub2unlock.com": "bypasses/sub2unlock.js",
+    "sub4unlock.com": "bypasses/sub4unlock.js",
+    "syosetu.org": "bypasses/syosetu.js",
+    "tik.lat": "bypasses/tiklat.js",
+    "uiz.io": "bypasses/uiz.js",
+    "uiz.app": "bypasses/uiz.js",
+    "tlkm.id": "bypasses/uiz.js",
+    "uploadrar.com": "bypasses/uploadrar.js",
+    "uploadrar.net": "bypasses/uploadrar.js",
+    "uptomega.me": "bypasses/uploadrar.js",
+    "vk.com/away.php?to=": "bypasses/vk.js",
+    "wadooo.com": "bypasses/wadooo.js",
+    "workink.click": "bypasses/workclick.js",
+    "work.ink": "bypasses/workink.js",
+    "ytsubme.com": "bypasses/ytsubme.js"
+};
+const script_src = new URL(document.currentScript.src);
+const ext_base_URL = script_src.searchParams.get('ext_base_URL');
+
+if (location.host in bypasses) {
+    const bypass_url = bypasses[location.host];
+
+    import(ext_base_URL.concat(bypass_url)).then(({default: bypass})=>{
+        import(ext_base_URL.concat('helpers/dom.js')).then(({default: helpers})=>{
+            const bps = new bypass();
+            bps.set_helpers(helpers);
+            console.log(`ensure_dom: ${bps.ensure_dom}`);
+            if (bps.ensure_dom) {
+                let executed = false;
+                document.addEventListener('readystatechange', ()=>{
+                    if (['interactive', 'complete'].includes(document.readyState) && !executed) {
+                        executed = true;
+                        bps.execute();
+                    }
+                }
+                );
+                document.addEventListener('DOMContentLoaded', ()=>{
+                    if (!executed) {
+                        executed = true;
+                        bps.execute();
+                    }
+                }
+                );
+            } else {
+                bps.execute();
+            }
+        }
+        );
+    }
+    );
+}
